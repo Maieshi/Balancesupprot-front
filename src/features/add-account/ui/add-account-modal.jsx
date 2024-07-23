ddcfce0bb287cf3d14eecfbe4dbaf046d0f91b5d
Akhmadov Muhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2024-07-23 13:48:11 +0500

    [u] updated add-account component compleated responsivity

--- src/features/add-account/ui/add-account-modal.jsx
+++ src/features/add-account/ui/add-account-modal.jsx
@@ -3,19 +3,35 @@ import { Modal } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { AddAccountPage } from './add-account-page.jsx'
 
+const sxStyle = {
+    width: {
+        xs: "70%",
+        sm: "50%",
+        md: "50%",
+        lg: '50%',
+        xl: '50%'
+    },
+    left: {
+        xs: '-90px',
+        sm: 0,
+        md: 0,
+        lg: 0,
+        xl: 0
+    }
+}
+
 export const AddAccountModal = () => {
     const dispatch = useDispatch();
     const modal = useSelector( ( state ) => state.addAccount.addAccount );
     return (
         <>
-            {/*<div className='add-account-modal'>*/ }
             <Modal
                 disableEnforceFocus
                 open={ modal }
                 onClose={ () => {
                     dispatch( openModal( false ) )
                 } }
-                sx={ {width: '50%'} }
+                sx={ sxStyle }
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
@@ -23,7 +39,6 @@ export const AddAccountModal = () => {
                     <AddAccountPage/>
                 </>
             </Modal>
-            {/*</div>*/ }
         </>
     )
         ;
