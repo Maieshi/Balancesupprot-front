2c64021082d40ae60260943cdb95f9f226365759
Akhmadov Muhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2024-07-25 22:22:15 +0500

    [u] updated ui

--- src/widgets/header/ui/header-component.jsx
+++ src/widgets/header/ui/header-component.jsx
@@ -4,7 +4,7 @@ import { MenuDrawer } from "@/features/menu-drawer";
 import { useDispatch } from 'react-redux'
 import { openModal } from "@/features/menu-drawer/modal/modal.js";
 import { useState } from "react";
-import { Menu, MenuItem } from "@mui/material";
+import { Menu, MenuItem, Divider } from "@mui/material";
 
 export const HeaderComponent = () => {
     const dispatch = useDispatch()
@@ -34,11 +34,22 @@ export const HeaderComponent = () => {
                 <Menu
                     sx={ {
                         '& .MuiPaper-root ': {
+                            borderRadius: "var(--border-radius)",
                             background: "var(--primary-color)",
-                            color: "#fff"
+                            color: "#fff",
+                            fontWeight: 700
                         }
                     } }
                     id="basic-menu"
+                    elevation={ 1 }
+                    anchorOrigin={ {
+                        vertical: 'bottom',
+                        horizontal: 'right',
+                    } }
+                    transformOrigin={ {
+                        vertical: 'top',
+                        horizontal: 'right',
+                    } }
                     anchorEl={ anchorEl }
                     open={ open }
                     onClose={ handleClose }
@@ -46,9 +57,12 @@ export const HeaderComponent = () => {
                         'aria-labelledby': 'basic-button',
                     } }
                 >
+                    <MenuItem>NickName</MenuItem>
+                    <Divider sx={{  borderColor: 'rgb(99, 115, 129)', }} />
+                    <MenuItem onClick={ handleClose }>Home</MenuItem>
                     <MenuItem onClick={ handleClose }>Profile</MenuItem>
-                    <MenuItem onClick={ handleClose }>My account</MenuItem>
-                    <MenuItem onClick={ handleClose }>Logout</MenuItem>
+                    <MenuItem onClick={ handleClose }>Settings</MenuItem>
+                    <MenuItem onClick={ handleClose } sx={ {color: "red", marginTop: "10px"} }>Logout</MenuItem>
                 </Menu>
             </header>
             <MenuDrawer/>
