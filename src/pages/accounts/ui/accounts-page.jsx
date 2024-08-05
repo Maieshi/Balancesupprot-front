582c27a1cdf181e13f93de999236187935031940
Akhmadov Muhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2024-08-05 08:19:56 +0500

    [f] fixed max width

--- src/pages/accounts/ui/accounts-page.jsx
+++ src/pages/accounts/ui/accounts-page.jsx
@@ -1,12 +1,14 @@
 import { Container } from "@mui/material";
 import { HeaderComponent } from "@/widgets/header";
-import { AccountHeader,AccountTable } from "@/widgets/account";
+import { AccountHeader, AccountTable } from "@/widgets/account";
 
 import './style.css'
 
 export const AccountsPage = () => {
     return (
-        <Container maxWidth="xl" className='account-container'>
+        <Container maxWidth={ false } sx={ {
+            maxWidth: "1900px"
+        } } className='account-container'>
             <HeaderComponent/>
             <AccountHeader/>
             <AccountTable/>
